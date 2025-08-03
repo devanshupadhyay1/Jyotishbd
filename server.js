0080a6c5e7a5f7f5607d2a98e51efb30da8bbe4e
devanshupadhyay1 <dpupadhyay292004@gmail.com>
2025-08-03 13:46:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(cors({origin:'jyotishfd-git-master-devanshs-projects-8bce964a.vercel.app'}));
+app.use(cors({origin:'jyotishfd-9a4pcooh2-devanshs-projects-8bce964a.vercel.app'}));
 app.use(express.json());
 
 // ✉️ Existing email route (untouched)
