827ef2987257bc6d78661a0d7726f7eb0dd1ce87
devanshupadhyay1 <dpupadhyay292004@gmail.com>
2025-08-03 14:01:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({origin:'https://jyotishfd-9a4pcooh2-devanshs-projects-8bce964a.vercel.app'}));
+app.use(cors({
+  origin:'https://jyotishfd-9a4pcooh2-devanshs--projects-8bce964a.vercel.app'}));
 app.use(express.json());
 
 // ✉️ Existing email route (untouched)
